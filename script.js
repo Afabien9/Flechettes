d1ede3c29e8f5a3a091242c9c597b33e08a43c23
alexisf <alexis.fabien45@gmail.com>
2025-12-16 23:00:44 +0100

    html et css minime

---
+++ script.js
@@ -0,0 +1,12 @@
+//constant of simple and triple
+const TARGET = [
+//Triple 
+    60, 57, 54, 51, 48, 45, 42, 39, 36, 33, 30, 27, 24, 21, 18, 15, 12, 9, 6, 3,
+//Simple and 25 is last because I don’t want him to be chosen first
+    20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 25
+];
+
+// constant of double for finish
+const DOUBLES = [
+    50, 40, 38, 36, 34, 32, 30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4, 2
+];
